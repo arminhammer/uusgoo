0d6c07ed47050c5f0e02d3d06b7a3406c9f578ae
arminhammer <arminhammer@gmail.com>
2015-03-23 20:29:36 -0400

    Added \d processing.

--- test/uusgoos_test.js
+++ test/uusgoos_test.js
@@ -9,10 +9,32 @@ describe('uusgoos', function () {
 
     uusgoos(['what', 'what', 'what'], function(pattern) {
 
-      pattern.should.equal('what')
+      pattern.should.equal('what');
 
     });
 
   });
+
+   it('should return a \d if there is a digit', function () {
+
+   uusgoos(['what1', 'what2', 'what3'], function(pattern) {
+
+   pattern.should.equal('what\\d');
+
+   });
+
+   });
+
+  /*
+  it('should return a \w if the strings are not the same, but are letters', function () {
+
+    uusgoos(['what', 'that'], function(pattern) {
+
+      pattern.should.equal('\\what');
+
+    });
+
+  });
+  */
 
 });
